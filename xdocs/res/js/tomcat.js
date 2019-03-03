6f969e79200ab6937476868e6d07828db515411a
Igal Sapir <isapir@apache.org>
2019-03-03 13:37:13 -0800

    updated layout to use bootstrap 4

--- xdocs/res/js/tomcat.js
+++ xdocs/res/js/tomcat.js
@@ -1,76 +1,44 @@
 /*
-* Copyright 2002-2018 The Apache Software Foundation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+  Licensed to the Apache Software Foundation (ASF) under one or more
+  contributor license agreements.  See the NOTICE file distributed with
+  this work for additional information regarding copyright ownership.
+  The ASF licenses this file to You under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with
+  the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
+$(function(){
 
-/** adds a live event handler akin to jQuery's on() */
-function addLiveEventListeners(selector, event, handler){
-    document.querySelector("body").addEventListener(
-         event
-        ,function(evt){
-            var target = evt.target;
-            while (target != null){
-                // isMatch is a patch for IE 9 - 11; once support for IE11
-                // is not required we can test with target.matches(selector)
-                var isMatch = target.matches ? target.matches(selector) : target.msMatchesSelector(selector);
-                if (isMatch){
-                    handler(evt);
-                    return;
-                }
-                target = target.parentElement;
-            }
-        }
-        ,true
-    );
-}
+  var $fontSizeSlider = $("#font-size");
+  $fontSizeSlider.val(windowFontSize);
 
+  $(window).scroll(function(e){
+    var sticky = 100;
+    var $body = $("body");
+    var top = $(this).scrollTop();
+    if (top == 0){
+      $body.removeClass("scrolled scrolled-sticky");
+    }
+    else {
+      $body.addClass("scrolled");
+      if (top >= sticky){
+        $body.addClass("scrolled-sticky");
+      }
+      else {
+        $body.removeClass("scrolled-sticky");
+      }
+    }
+  });
 
-/** adds event listener to the elements that match the selector */
-function addEventListeners(selector, event, handler, useCapture){
-    [].forEach.call(
-        document.querySelectorAll(selector)
-       ,function(el, ix) {
-          el.addEventListener(event, handler, useCapture || false);
-        }
-    );
-}
-
-
-/** toggles a class for all of the nodes that match a selector akin to jQuery's toggleClass() */
-function toggleClass(selector, className){
-    var nodes = document.querySelectorAll(selector);
-    [].forEach.call(nodes, function(node) {
-        if (node.classList.contains(className))
-            node.classList.remove(className);
-        else
-            node.classList.add(className);
-    });
-}
-
-
-function toggleMenu(){
-    toggleClass("#mainLeft", "opened");
-    toggleClass(".menu-toggler", "opened");
-}
-
-
-addEventListeners(".menu-toggler", "click", function(evt){
-    toggleMenu();
-});
-
-
-addEventListeners(".menu-toggler", "keyup", function(evt){
-    if (evt.which == 13)
-        toggleMenu();
-});
+  $fontSizeSlider.on("change", function(e){
+    setFontsize($(this).val());
+  });
+  
+}); // ready
